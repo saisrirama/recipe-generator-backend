bfa58fde7e68a9bc1ee5ea2f99cf45a92875e24c
saisrirama <saisrirama.apk@gmail.com>
2026-01-01 18:21:28 +0000

    Update allowed origins in CORS configuration

--- src/main/java/com/springai/recipegen/WebConfig.java
+++ src/main/java/com/springai/recipegen/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
            @Override
            public void addCorsMappings(CorsRegistry registry) {
                registry.addMapping("/**")
-                       .allowedOrigins("http://localhost:3000/","https://saisrirama.github.io/recipe-generator/")
+                       .allowedOrigins("http://localhost:3000/","https://saisrirama.github.io")
                        .allowedMethods("GET","POST","PUT", "DELETE", "OPTIONS")
                        .allowedHeaders("*")
                        .allowCredentials(true);
