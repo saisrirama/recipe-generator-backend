06668d9da3f9fd619ac0064bebdf11f92502f81a
saisrirama <saisrirama.apk@gmail.com>
2026-01-02 18:46:28 +0000

    Refactor CORS configuration in WebConfig

--- src/main/java/com/springai/recipegen/WebConfig.java
+++ src/main/java/com/springai/recipegen/WebConfig.java
@@ -1,6 +1,5 @@
 package com.springai.recipegen;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,17 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-   @Bean
-    public WebMvcConfigurer corsConfigurer(){
-       return new WebMvcConfigurer() {
-           @Override
-           public void addCorsMappings(CorsRegistry registry) {
-               registry.addMapping("/**")
-                       .allowedOrigins("http://localhost:3000/","https://saisrirama.github.io")
-                       .allowedMethods("GET","POST","PUT", "DELETE", "OPTIONS")
-                       .allowedHeaders("*")
-                       .allowCredentials(true);
-           }
-       };
-   }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://saisrirama.github.io"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
