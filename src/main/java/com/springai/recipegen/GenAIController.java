e3d78770e118a6540d018d6b005ff9091d866b07
KarlyleFr <karlyle.fr@gmail.com>
2026-02-17 14:56:13 +0000

    added favorite tab

--- src/main/java/com/springai/recipegen/GenAIController.java
+++ src/main/java/com/springai/recipegen/GenAIController.java
@@ -16,27 +16,29 @@ public class GenAIController {
     }
 
     @GetMapping("ask-ai")
-    public String getResponse(@RequestParam String prompt){
+    public String getResponse(@RequestParam String prompt) {
         return chatService.getResponse(prompt);
     }
 
     @GetMapping("ask-ai-options")
-    public String getResponseOptions(@RequestParam String prompt){
+    public String getResponseOptions(@RequestParam String prompt) {
         return chatService.getResponseOptions(prompt);
     }
 
-    /*@GetMapping("generate-image")
-    public void generateImages(HttpServletResponse response, @RequestParam String prompt) throws IOException {
-        ImageResponse imageResponse = imageService.generateImage(prompt);
-        String imageUrl = imageResponse.getResult().getOutput().getUrl();
-        response.sendRedirect(imageUrl);
-    }*/
-
+    /*
+     * @GetMapping("generate-image")
+     * public void generateImages(HttpServletResponse response, @RequestParam String
+     * prompt) throws IOException {
+     * ImageResponse imageResponse = imageService.generateImage(prompt);
+     * String imageUrl = imageResponse.getResult().getOutput().getUrl();
+     * response.sendRedirect(imageUrl);
+     * }
+     */
 
     @GetMapping("recipe-generator")
     public String recipeCreator(@RequestParam String ingredients,
-                                      @RequestParam(defaultValue = "any") String cuisine,
-                                      @RequestParam(defaultValue = "") String dietaryRestriction) {
-        return recipeService.createRecipe(ingredients, cuisine, dietaryRestriction);
+            @RequestParam(defaultValue = "any") String cuisine,
+            @RequestParam(defaultValue = "") String dietaryRestrictions) {
+        return recipeService.createRecipe(ingredients, cuisine, dietaryRestrictions);
     }
 }
